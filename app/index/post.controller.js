a87445cc590d2ae31843003d700fb2c5ac232c85
Jon Heslop <jon@jonheslop.com>
2019-07-02 10:54:07 +0100

    Remove usused routes from other app

--- app/index/post.controller.js
+++ app/index/post.controller.js
@@ -2,18 +2,16 @@
 
 // Local dependencies
 const { validator } = require('../../common/utils/field-validator')
-const { paths: somethingsWrong } = require('../somethings-wrong/index')
-const { paths: help } = require('../help/index')
 
 module.exports = (req, res) => {
   const supportType = req.body['support-type']
 
   if (supportType === 'somethings-wrong') {
-    return res.redirect(somethingsWrong.index)
+    return res.redirect('/')
   }
 
   if (supportType === 'help') {
-    return res.redirect(help.index)
+    return res.redirect('/')
   }
 
   const errors = validator([
